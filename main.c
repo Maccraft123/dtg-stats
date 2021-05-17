c6e74a984971fdda935620ca63b78aa4a4d1a8fe
Maciej Matuszczyk <maccraft123mc@gmail.com>
2021-05-17 08:26:07 +0000

    fixes 2

--- main.c
+++ main.c
@@ -9,32 +9,31 @@ int main(void)
 	int vol, soc, tmp;
 	char out[7];
 
-	batfd = fopen("/sys/class/power_supply/rk817-battery/voltage_now/", "r");
-	tmp = fscanf(batfd, "%d", &vol);
-	if(tmp)
-		return 1;
+	batfd = fopen("/sys/class/power_supply/rk817-battery/voltage_now", "r");
+	fscanf(batfd, "%d", &vol);
 	fclose(batfd);
 
 	soc = (vol - 3500000) / 6000;
 
 	switch(soc)
 	{
-		case 91 ... 100: // [FUL]+
+		case 91 ... 100:
 			strcpy(out, "[FUL]+");
 			break;
-		case 76 ... 80:  // [---]+
+		case 76 ... 80:
 			strcpy(out, "[---]+");
 			break;
-		case 36 ... 75:  // [-- ]+
+		case 36 ... 75:
 			strcpy(out, "[-- ]+");
 			break;
-		case 11 ... 35:  // [-  ]+
+		case 11 ... 35:
 			strcpy(out, "[-  ]+");
 			break;
-		case 0 ... 10:	 // [   ]+
+		case 0 ... 10:
 			strcpy(out, "[   ]+");
 			break;
 	}
-	printf("%s\n", out);
+	printf("%s", out);
+	fflush(stdout);
 	return 0;
 }
